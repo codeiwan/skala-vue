f219c133e67d6da71068b5dc3b94623774bc05ff
codeiwan <pewlv7976@gmail.com>
2026-08-19 14:46:57 +0900

    skala-vue practice project init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+
+import VueIf from './components/practices/basic/VueIf.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
+
+import EventBasic from './components/practices/basic/EventBasic.vue'
+import EventObject from './components/practices/basic/EventObject.vue'
+import EventModifier from './components/practices/basic/EventModifier.vue'
+
+import ModelBasic from './components/practices/basic/ModelBasic.vue'
+import ModerForm from './components/practices/basic/ModelForm.vue'
+import ModelModifier from './components/practices/basic/ModelModifier.vue'
+import StyleScoped from './components/practices/basic/StyleScoped.vue'
+</script>
+
+<template>
+  <div style="padding: 20px">
+    <SampleOne />
+    <SampleTwo />
+
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+
+    <VueIf />
+    <VueShow />
+    <VueFor />
+
+    <EventBasic />
+    <EventObject />
+    <EventModifier />
+
+    <ModelBasic />
+    <ModerForm />
+    <ModelModifier />
+    <StyleScoped />
+  </div>
+</template>
+
+<style scoped>
+@import '@/assets/practice.css';
+</style>
